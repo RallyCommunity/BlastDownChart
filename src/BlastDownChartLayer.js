3cb458f423d054edd4cb45868aefd7399a74e14e
Matt Greer <matt.e.greer@gmail.com>
2012-07-25 20:58:45 -0600

    ships now bob back and forth

--- src/BlastDownChartLayer.js
+++ src/BlastDownChartLayer.js
@@ -3,19 +3,21 @@ var geom = require('geometry');
 
 var Player = require('/Player');
 
+
 function BlastDownChartLayer(script) {
 	BlastDownChartLayer.superclass.constructor.call(this);
 
-	var s = cocos.Director.sharedDirector.winSize;
+	var winSize = cocos.Director.sharedDirector.winSize;
 
 	var player = new Player();
-	player.position = new geom.Point(s.width / 2, 100);
+	player.position = new geom.Point(winSize.width / 2, 40);
 	this.addChild(player);
 	this.player = player;
 
 	this.script = script;
-	this.scheduleUpdate();
 	this.elapsed = 0;
+
+	this.scheduleUpdate();
 }
 
 BlastDownChartLayer.inherit(cocos.nodes.Layer, {
@@ -37,15 +39,26 @@ BlastDownChartLayer.inherit(cocos.nodes.Layer, {
 	},
 
 	_positionShip: function(ship) {
-		this._shipY = this._shipY || 5;
-		ship.position = new geom.Point(20, this._shipY);
-		this._shipY += ship.contentSize.height + 10;
+		var winSize = cocos.Director.sharedDirector.winSize;
+
+		this._shipY = this._shipY || winSize.height - 20;
+		ship.position = new geom.Point(60, this._shipY);
+		this._shipY -= ship.contentSize.height + 20;
+	},
+
+	_setBobbing: function(ship) {
+		var jumpRight = new cocos.actions.JumpBy({ duration: 0.5, delta: new geom.Point(14, 0),   height: -8, jumps: 1 });
+		var jumpLeft = jumpRight.reverse();
+		var jumpSequence = new cocos.actions.Sequence({actions: [jumpRight, jumpLeft] });
+
+		ship.runAction(new cocos.actions.RepeatForever(jumpSequence));
 	},
 
 	_spawn: function(config) {
 		var ship = new config.type();
 		ship.tag = config.id;
 		this._positionShip(ship);
+		this._setBobbing(ship);
 		//ship.runAction(new EmergeFrom(config.from));
 		this.addChild(ship);
 	},
