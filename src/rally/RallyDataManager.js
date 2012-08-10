50cae58d661f736358dabe7f9836999c8d2d860d
Charles <Ferentchak@gmail.com>
2012-08-10 09:24:33 -0600

    Fix query

--- src/rally/RallyDataManager.js
+++ src/rally/RallyDataManager.js
@@ -4,13 +4,14 @@ var e = {};
 e.getData = function(callback) {
     Ext.onReady(function() {
         var find = {
-            "_PreviousValues.PercentDone":{
-                $ne:100
-            },
-            ScheduleState:"Accepted",
-            _ItemHierarchy:5401627848
-
-        };
+            $and: [
+                { _ItemHeirarchy: 5401627848 },
+                {$or: [
+                    {_SnapshotNumber :0, ObjectID: {$ne: 5401627848} },
+                    { "_PreviousValues.Blocked" : {$exists:true} },
+                    { ObjectID: 5401627848, PercentDoneByStoryCount : 1}
+                ]}
+            ]};
         find = Ext.JSON.encode(find);
         Ext.Ajax.request({
             method:"get",
@@ -19,8 +20,7 @@ e.getData = function(callback) {
                 pagesize: "10",
                 find:find,
                 fields:'["_PreviousValues","ScheduleState","_ItemHierarchy"]',
-                sort:'{_ValidFrom:1}',
-                hydrate:["ScheduleState"]
+                sort:'{_ValidFrom:1}'
             },
 
             cors:true,
